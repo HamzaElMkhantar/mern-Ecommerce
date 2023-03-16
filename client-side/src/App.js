8ac3f04738ac7c29264c829408b4e631cb2c4f4e
Hamza El Mkhantar <hamzaelmkhantar4@gmail.com>
2023-03-16 14:50:02 +0100

    initial structure

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Footer from './Footer';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      hello react
+      <Header/>
+      <h1>
+        hello react
+      </h1>
+      <Footer />
     </div>
   );
 }
